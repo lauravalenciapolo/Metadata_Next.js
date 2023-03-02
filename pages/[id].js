45c5a02789450e6ce60a8fcbd351d3a0c62a6b19
unknown <lauravalenciapolo@gmail.com>
2023-03-01 20:51:18 -0500

    peticion axios stalak

--- pages/[id].js
+++ pages/[id].js
@@ -1,22 +1,16 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
 import axios from 'axios'
 
-const inter = Inter({ subsets: ['latin'] })
 
 export async function getServerSideProps(context) {
   const { req, res } = context;
-  const {id} = context.params
+  const {id} = context.query
+  const response = await axios.get(`http://34.204.134.151:4000/api/public/posts?post_id=11`);
 
-  // Fetch data from your service using req and res
-  const response = await axios.get(`https://rickandmortyapi.com/api/character/${id}`);
-
-  // Extract the data from the response
   const data = response.data;
 
-  // Return the data as props
   return {
     props: {
       data
@@ -25,23 +19,24 @@ export async function getServerSideProps(context) {
 }
 
 export default function Post(props) {
-  console.log(props.data);
+  console.log(props.data, "holaa");
 
   return (
     <>
       <Head>
         <title>Stalak</title>
-        <meta name="description" content={props.data.name} />
+        <meta name="description" content={props.data.data.habit_name} />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta property="og:url" content="https://stalak.com"></meta>
         <meta property="og:image"
-        content={props.data.image}
+        content={props.data.data.image_url}
         />
         <meta name="og:title" content="Stalak" />
         <meta name="twitter:card" content="summary_large_image" />
       </Head>
-      <h1>Post id: {props.data.id}</h1>
-      <h3>Usuario: {props.data.name}</h3>
+      <h1>Post id: {props.data.data.id}</h1>
+      <h3>Usuario: {props.data.data.user.name}</h3>
+      <Image src={props.data.data.image_url} alt={props.data.data.habit_name} width={500} height={500} />
     </>
   )
 }
--- pages/index.js
+++ pages/index.js
@@ -1,29 +1,12 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
+import Image from 'next/image';
 
-const inter = Inter({ subsets: ['latin'] })
 
-export default function Home(props) {
-  
-  console.log(props.data);
+export default function Home() {
 
   return (
     <>
-      {/* <Head>
-        <title>Stalak</title>
-        <meta name="description" content={props.data.name} />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
-        <meta property="og:image"
-        //content="https://rickandmortyapi.com/api/character/avatar/1.jpeg"
-        content={props.data.image}
-        />
-        <meta name="og:title" content="Stalak" />
-        <meta name="twitter:card" content="summary_large_image" />
-      </Head> */}
-      <h1>Bienvenidos a Stalak</h1>
+      <Image src="/stalak.png" alt="Stalak" width={500} height={500} />
     </>
   )
 }
