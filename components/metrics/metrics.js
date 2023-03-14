4fe8e2d8bdf667650ffc26d2468d2b1750740f62
unknown <lauravalenciapolo@gmail.com>
2023-03-14 14:13:52 -0500

    landing responsive

--- components/metrics/metrics.js
+++ components/metrics/metrics.js
@@ -9,7 +9,7 @@ export default function Metrics() {
             <h1>Quiero ayudarte a crear hábitos saludables que te impulsen a alcanzar tus objetivos</h1>
             </div>
             <div className={styles.contentInfo}>
-                <Image src="/visuals2.png" alt="Stalak" width={378} height={378} className={styles.myImage} />
+                <Image src="/Visuals2.png" alt="Stalak" width={378} height={378} className={styles.myImage} />
                 <div className={styles.parrag}>
                 <p>Te ayudo a compartir tus hábitos saludables con tus amigos y conocidos, así aprendes de las experiencias de otros y construyes una comunidad parecida a ti</p>
                 </div>
@@ -18,10 +18,10 @@ export default function Metrics() {
                 <div className={styles.parrag}>
                 <p>Soy una red social en la que puedes conocer nuevas personas a partir de un gusto común, además puedes unirte a los clubes y practicar tus actividades favoritas</p>
                 </div>
-                <Image src="/visuals2.png" alt="Stalak" width={378} height={378} className={styles.myImage2} />
+                <Image src="/Visuals2.png" alt="Stalak" width={378} height={378} className={styles.myImage2} />
             </div>
             <div className={styles.contentInfo} style={{top:"3174px"}}>
-                <Image src="/visuals2.png" alt="Stalak" width={378} height={378} className={styles.myImage} />
+                <Image src="/Visuals2.png" alt="Stalak" width={378} height={378} className={styles.myImage} />
                 <div className={styles.parrag}>
                 <p>Conecta con personas en diferentes lugares para planificar y ejecutar actividades en común que te hagan feliz y te ayuden a llevar una vida saludable</p>
                 </div>
