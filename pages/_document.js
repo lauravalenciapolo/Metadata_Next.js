adf03d85e187c3d1034f84514ddeb6677eb968f2
unknown <lauravalenciapolo@gmail.com>
2023-03-16 17:18:02 -0500

    ensayando imagen

--- pages/_document.js
+++ pages/_document.js
@@ -3,11 +3,22 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
-      <body style={{backgroundColor:"#F8F9FF"}}>
+      <Head>
+        <title>Stalak</title>
+        <meta
+          name="description"
+          content="Soy una red social que te acompaña a todos lados, te ayudo a establecer metas que se conviertan en hábitos para organizar tu vida."
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/Group_5.png" />
+        <meta property="og:image" content="/stalak_logo.png" />
+        <meta name="og:title" content="Stalak" />
+        <meta property="og:url" content="https://stalak.com"></meta>
+      </Head>
+      <body style={{ backgroundColor: "#F8F9FF" }}>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
