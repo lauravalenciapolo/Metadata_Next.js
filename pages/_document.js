4aed949aa93225e755ef58679efa32dc295fa87d
unknown <lauravalenciapolo@gmail.com>
2023-03-28 11:28:17 -0500

    ajustes metadata

--- pages/_document.js
+++ pages/_document.js
@@ -15,7 +15,7 @@ export default function Document() {
           content="Soy una red social que te acompaña a todos lados, te ayudo a establecer metas que se conviertan en hábitos para organizar tu vida."
         />
         <link rel="icon" href="/Group_5.png" />
-        <meta property="og:image" itemprop="image" content="/imageog2.png" />
+        <meta property="og:image" itemprop="image" content="/imageog22.png" />
         <meta property="og:image:type" content="image/png"></meta>
         <meta property="og:image:height" content="1000" />
         <meta property="og:image:width" content="1000" />
